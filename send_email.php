e5eb5ed8c334073a9edb8c5b6a79253a8f8d8a01
Dhanashree-02 <dhanashree.sonune02@gmail.com>
2024-09-02 01:17:29 -1000

    index

--- send_email.php
+++ send_email.php
@@ -1,22 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullName = $_POST['fullName'];
-    $mobileNumber = $_POST['mobileNumber'];
-    $emailAddress = $_POST['emailAddress'];
-    $selectDate = $_POST['selectDate'];
-    $pickupPoint = $_POST['pickupPoint'];
-    $trekkersNumber = $_POST['trekkersNumber'];
-    $totalPayment = $_POST['totalPayment'];
-
-    $to = "info@matoshreeholidaystoursandtravels.com"; // Replace with your email address
-    $subject = "New Booking Form Submission";
-    $message = "Full Name: $fullName\nMobile Number: $mobileNumber\nEmail Address: $emailAddress\nDate: $selectDate\nPickup Point: $pickupPoint\nNumber of Trekkers: $trekkersNumber\nTotal Payment: $totalPayment";
-    $headers = "From: $emailAddress";
-
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Mail Sent Successfully!";
-    } else {
-        echo "Mail Sending Failed!";
-    }
+  $fullName = $_POST['fullName'];
+  $mobileNumber = $_POST['mobileNumber'];
+  $emailAddress = $_POST['emailAddress'];
+  $selectDate = $_POST['selectDate'];
+  $pickupPoint = $_POST['pickupPoint'];
+  $trekkersNumber = $_POST['trekkersNumber'];
+  
+  $to = "info@matoshreeholidaystoursandtravels.com"; // Replace with your email
+  $subject = "New Booking";
+  $message = "Full Name: $fullName\n";
+  $message .= "Mobile Number: $mobileNumber\n";
+  $message .= "Email Address: $emailAddress\n";
+  $message .= "Select Date: $selectDate\n";
+  $message .= "Pickup Point: $pickupPoint\n";
+  $message .= "Number of Trekkers: $trekkersNumber\n";
+  
+  // Send email
+  if (mail($to, $subject, $message)) {
+    echo "Email sent successfully!";
+  } else {
+    echo "Failed to send email.";
+  }
 }
 ?>
